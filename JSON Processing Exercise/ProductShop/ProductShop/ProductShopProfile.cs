083234e62887165bde996bf1de2bd20a7bddfcd2
David Nakov <nakov_david@mail.bg>
2023-03-12 14:44:50 +0200

    feat: json processing exercise task: first project - Product Shop.

---
+++ JSON Processing Exercise/ProductShop/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ProductShop.DTOs.Export;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile() 
+        {
+            CreateMap<Product, ProductDto>()
+                .ForMember(x => x.Seller, y => y.MapFrom(s => $"{s.Seller.FirstName ?? ""} {s.Seller.LastName}"))
+                .ForMember(x => x.Price, y => y.MapFrom(s => s.Price));
+
+            CreateMap<Category, CategoryDTO>()
+                .ForMember(x => x.Category, y => y.MapFrom(s => s.Name))
+                .ForMember(x => x.ProductsCount, y => y.MapFrom(s => s.CategoriesProducts.Count))
+                .ForMember(x => x.AveragePrice, y => y.MapFrom(s => decimal.Parse(s.CategoriesProducts.Average(cp => cp.Product.Price).ToString("f2"))))
+                .ForMember(x => x.TotalRevenue, y => y.MapFrom(s => decimal.Parse(s.CategoriesProducts.Sum(cp => cp.Product.Price).ToString("f2"))));
+
+        }
+    }
+}
---
+++ JSON Processing Exercise/ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,154 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.DTOs.Export;
+using ProductShop.Models;
+using System.Globalization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new ProductShopContext();
+
+            var result = GetCategoriesByProductsCount(db);
+
+            Console.WriteLine(result);
+        }
+
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+
+            context.Users.AddRange(users!);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {users!.Count}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            var products = JsonConvert.DeserializeObject<List<Product>>(inputJson);
+
+            context.Products.AddRange(products!);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {products!.Count}";
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+
+            var categories = JsonConvert.DeserializeObject<IEnumerable<Category>>(inputJson);
+
+            categories = categories!.Where(c => c.Name != null);
+
+            context.Categories.AddRange(categories);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Count()}";
+        }
+
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        {
+            var categoriesProducts = JsonConvert.DeserializeObject<List<CategoryProduct>>(inputJson);
+
+            context.CategoriesProducts.AddRange(categoriesProducts!);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {categoriesProducts!.Count}";
+        }
+
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<ProductShopProfile>());
+
+            var productsToExport = context.Products
+                .Where(p => p.Price >= 500 && p.Price <= 1000)
+                .OrderBy(p => p.Price)
+                .ProjectTo<ProductDto>(config)
+                .ToList();
+
+            var json = JsonConvert.SerializeObject(productsToExport);
+
+            return json;
+        }
+
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<ProductShopProfile>());
+
+            var dataToExport = context.Users
+                .Where(u => u.ProductsSold.Any(p => p.Buyer != null))
+                .OrderBy(u => u.LastName)
+                .ThenBy(u => u.FirstName)
+                .Select(u => new
+                {
+                    firstName = u.FirstName,
+                    lastName = u.LastName,
+                    soldProducts = u.ProductsSold.Select(p => new
+                    {
+                        name = p.Name,
+                        price = p.Price,
+                        buyerFirstName = p.Buyer.FirstName,
+                        buyerLastName = p.Buyer.LastName
+                    })
+                })
+                .ToList();
+
+            var json = JsonConvert.SerializeObject(dataToExport);
+
+            return json;
+        }
+
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<ProductShopProfile>());
+
+            var dataToExport = context.Categories
+                .OrderByDescending(c => c.CategoriesProducts.Count)
+                .ProjectTo<CategoryDTO>(config)
+                .ToArray();
+
+            var json = JsonConvert.SerializeObject(dataToExport, Formatting.Indented);
+
+            return json;
+        }
+
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+            var dataToExport = context.Users
+                .Where(u => u.ProductsSold.Any(p => p.Buyer != null))
+                .OrderByDescending(u => u.ProductsSold.Count)
+                .Select(u => new
+                {
+                    lastName = u.LastName,
+                    age = u.Age,
+                    soldProducts = new
+                    {
+                        count = u.ProductsSold.Count,
+                        products = u.ProductsSold.Select(p => new
+                        {
+                            name = p.Name,
+                            price = decimal.Parse(p.Price.ToString("f2"))
+                        })
+                    }
+                })
+                .ToArray();
+
+            var json = JsonConvert.SerializeObject(new { usersCount = dataToExport.Length, users = dataToExport }, Formatting.Indented);
+
+            return json;
+        }
+    }
+}
